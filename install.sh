d9ebea7bef69d20b1ea100bc4b77ece91b465172
jmpn89 <46716786+jmpn89@users.noreply.github.com>
2024-10-02 11:58:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 
-sudo apt-get update && sudo apt-get install python-rpi.gpio python3-rpi.gpio pip
-pip install RPi.GPIO
+sudo apt-get update && sudo apt-get install python3-rpi.gpio python-rpi.gpio # pip
+#pip install RPi.GPIO
 
 sudo cp fanctrl.service /lib/systemd/system
 sudo systemctl enable fanctrl.service
+sudo systemctl start fanctrl.service
+sudo systemctl status fanctrl.service
